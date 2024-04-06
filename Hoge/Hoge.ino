6162b4662a077f12c59825291c2e949f71a26569
Akira SEKIGUCHI <s224999z@st.me.tuat.ac.jp>
2024-04-06 18:41:13 +0900

    add: テンプレスケッチにループ時間調整コードを追加
    よく使うコードなので、コピペしてくる手間を省けるように

--- Hoge/Hoge.ino
+++ Hoge/Hoge.ino
@@ -1,3 +1,18 @@
-void setup() {}
+int dataRate = 100;
+uint32_t loopDuration() { return 1'000'000 / dataRate; }
+uint32_t loopTimer;
+uint32_t nowTime;
+void setup() { loopTimer = micros(); }
 
-void loop() {}
+void loop() {
+    nowTime = micros();
+    //
+    // loop content here
+    //
+    uint32_t currentTime = micros();
+    uint32_t duration    = loopDuration();
+    while (currentTime - looptimer <= duration) {
+        currentTime = micros();
+    }
+    loopTimer = currentTime;
+}
